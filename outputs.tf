3cce9051fd475fe214be0c140bd924ce1dec5d7a
Cristian Beraha <cristian@cloudops.works>
2026-01-19 17:38:43 -0300

    chore: Add outputs for service accounts and custom roles

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,23 @@
 #     Distributed Under Apache v2.0 License
 #
 
+output "service_accounts" {
+  value = {
+    for sa_key, sa in local.sa_map : sa_key => {
+      email        = google_service_account.sa[sa_key].email
+      unique_id    = google_service_account.sa[sa_key].unique_id
+      display_name = google_service_account.sa[sa_key].display_name
+      description  = google_service_account.sa[sa_key].description
+    }
+  }
+}
+
+output "roles" {
+  value = {
+    for role_key, role in local.project_roles_map : role_key => {
+      name        = google_project_iam_custom_role.project_role[role_key].name
+      title       = google_project_iam_custom_role.project_role[role_key].title
+      description = google_project_iam_custom_role.project_role[role_key].description
+    }
+  }
+}
